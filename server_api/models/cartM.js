942f2a149d12150867c04a16b99ba34395206037
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-04 11:40:04 +0700

    create model cart

---
+++ server_api/models/cartM.js
@@ -0,0 +1,21 @@
+const Mongoose = require("mongoose");
+
+const cartSchema = new Mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    products: [
+        {
+            productId: {
+                type: String,
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            },
+        }
+    ]
+}, { timestamps: true });
+
+module.exports = Mongoose.model("Cart", cartSchema)
