d415b6699a0694e6706803c9f5f12b7575b7ed01
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-26 16:32:34 +0700

    create endpoint update and delete

--- server_api/routes/productR.js
+++ server_api/routes/productR.js
@@ -15,31 +15,26 @@ router.post('/', verufyTokenAndAdmin, async (req, res) => {
 })
 
 // Update
-
-// router.put('/:id', verufyTokenAndAuthorization, async (req, res) => {
-//     if (req.body.password) {
-//         req.body.password = cryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
-//     }
-//     try {
-
-//         const updateUser = await userM.findByIdAndUpdate(req.params.id, {
-//             $set: req.body
-//         }, { new: true })
-//         res.status(200).json(updateUser)
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+router.put('/:id', verufyTokenAndAdmin, async (req, res) => {
+    try {
+        const updateProduct = await productM.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
+        res.status(200).json(updateProduct)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // Delete
-// router.delete('/:id', verufyTokenAndAuthorization, async (req, res) => {
-//     try {
-//         const deleteUser = await userM.findByIdAndDelete(req.params.id)
-//         res.status(200).json('User has been delete...')
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+router.delete('/:id', verufyTokenAndAdmin, async (req, res) => {
+    try {
+        await productM.findByIdAndDelete(req.params.id)
+        res.status(200).json('Product has been delete...')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // Get User Me
 // router.get('/find/:id', verufyTokenAndAdmin, async (req, res) => {
