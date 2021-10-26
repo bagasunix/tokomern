5edb144e9ac7461cc18b0b27a0e91d124f1fed90
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-26 17:18:02 +0700

    create endpoint get all

--- server_api/routes/productR.js
+++ server_api/routes/productR.js
@@ -51,47 +51,29 @@ router.get('/find/:id', verufyTokenAndAdmin, async (req, res) => {
 
 
 // Get All User
-router.get('/', verufyTokenAndAdmin, async (req, res) => {
-    const query = req.query.new;
+router.get('/', async (req, res) => {
+    const qNew = req.query.new;
+    const qcategory = req.query.category;
     try {
-        const users = query ? await productM.find().sort({ _id: -1 }).limit(10) : await productM.find()
-
+        let products;
+        if (qNew) {
+            products = await productM.find().sort({ createdAt: -1 }).limit(10)
+        } else if (qcategory) {
+            products = await productM.find({
+                categories: {
+                    $in: [qcategory]
+                }
+            });
+        } else {
+            products = await productM.find()
+        }
         return res.status(200).json({
             status: 'success',
-            data: users
+            data: products
         })
     } catch (err) {
         res.status(500).json(err)
     }
 })
-
-// Get User Stat
-// router.get('/stats', verufyTokenAndAdmin, async (req, res) => {
-//     const date = new Date()
-//     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1))
-//     console.log(date, lastYear);
-//     try {
-//         const data = await userM.aggregate([
-//             { $match: { createdAt: { $gte: lastYear } } },
-//             {
-//                 $project: {
-//                     month: { $month: "$createdAt" }
-//                 }
-//             },
-//             {
-//                 $group: {
-//                     _id: "$month",
-//                     total: { $sum: 1 }
-//                 }
-//             }
-//         ])
-//         return res.status(200).json({
-//             status: 'success',
-//             data
-//         })
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
 
 module.exports = router
