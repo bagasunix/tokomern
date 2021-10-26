58f36a3a93ff1f83efc344a853ef7609c3f6d621
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-26 23:16:53 +0700

    create route order

--- server_api/index.js
+++ server_api/index.js
@@ -7,6 +7,7 @@ const userRouter = require('./routes/userR')
 const authRouter = require('./routes/auth')
 const productRouter = require('./routes/productR')
 const cartRouter = require('./routes/cartR')
+const orderRouter = require('./routes/orderR')
 
 mongoose
     .connect(process.env.DB, {
@@ -21,8 +22,9 @@ app.use(express.json())
 
 app.use("/api/users", userRouter)
 app.use("/api/auth", authRouter)
-app.use("/api/product", productRouter)
-app.use("/api/cart", cartRouter)
+app.use("/api/products", productRouter)
+app.use("/api/carts", cartRouter)
+app.use("/api/orders", orderRouter)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
