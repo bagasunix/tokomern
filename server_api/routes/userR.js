aa274a1401a3aaeb6dfe66865631b90790928f29
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-26 15:03:49 +0700

    create endpoint find user with jwt

--- server_api/routes/userR.js
+++ server_api/routes/userR.js
@@ -1,7 +1,9 @@
 const userM = require('../models/userM')
-const { verifyToken, verufyTokenAndAuthorization } = require('./verifyToken')
+const { verifyToken, verufyTokenAndAuthorization, verufyTokenAndAdmin } = require('./verifyToken')
 
 const router = require('express').Router()
+
+// Update
 
 router.put('/:id', verufyTokenAndAuthorization, async (req, res) => {
     if (req.body.password) {
@@ -13,6 +15,31 @@ router.put('/:id', verufyTokenAndAuthorization, async (req, res) => {
             $set: req.body
         }, { new: true })
         res.status(200).json(updateUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// Delete
+router.delete('/:id', verufyTokenAndAuthorization, async (req, res) => {
+    try {
+        const deleteUser = await userM.findByIdAndDelete(req.params.id)
+        res.status(200).json('User has been delete...')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// Get User Me
+router.get('/find/:id', verufyTokenAndAdmin, async (req, res) => {
+    try {
+        const userMe = await userM.findById(req.params.id)
+        const { password, __v, ...others } = userMe._doc
+
+        return res.status(200).json({
+            status: 'success',
+            data: others
+        })
     } catch (err) {
         res.status(500).json(err)
     }
