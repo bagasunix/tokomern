b44a7d2283f6712f9ffc698a0eeaeb8a1a170f11
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-26 14:27:31 +0700

    create endpoint update with jtw

--- server_api/routes/userR.js
+++ server_api/routes/userR.js
@@ -1,14 +1,21 @@
+const userM = require('../models/userM')
+const { verifyToken, verufyTokenAndAuthorization } = require('./verifyToken')
+
 const router = require('express').Router()
 
+router.put('/:id', verufyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = cryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    }
+    try {
 
-router.get("/", (req, res) => {
-    res.send("Test is Successfully");
+        const updateUser = await userM.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
+        res.status(200).json(updateUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
-router.post('/regist', (req, res) => {
-    const user= req.body.username;
-    console.log(user);
-    res.send(user)
-})
-
-module.exports = router 
+module.exports = router
