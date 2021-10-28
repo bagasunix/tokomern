478b6e401b40fabb7cf44f743ee69547bb752357
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-28 16:49:09 +0700

    create product

--- clients/src/pages/Home.jsx
+++ clients/src/pages/Home.jsx
@@ -3,14 +3,16 @@ import Announcement from '../components/Announcement'
 import { Categories } from '../components/Categories'
 import { Navbar } from '../components/Navbar'
 import { Slider } from '../components/Slider'
+import Products from '../components/Products'
 
-const Home = () => {
+export const Home = () => {
     return (
         <div>
             <Announcement/>
             <Navbar />
             <Slider />
-            <Categories/>
+            <Categories />
+            <Products/>
         </div>
     )
 }
