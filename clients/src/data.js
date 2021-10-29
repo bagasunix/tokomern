672f0ee83ad0d940b8a033965ad9ef07a7c570e1
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-29 15:12:29 +0700

    change img slider

--- clients/src/data.js
+++ clients/src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
     {
         id: 1,
-        img: "React",
+        img: "https://i.ibb.co/cXFnLLV/3.png",
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "f5fafd",
