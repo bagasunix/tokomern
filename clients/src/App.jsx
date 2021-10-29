b6be1e0e3e306e34c118aafe3f3f1a636d34c1fd
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-29 17:24:32 +0700

    add route

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,35 +1,30 @@
-import {ProductList} from "./pages/ProductList";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
-import {Cart} from "./pages/Cart";
-import Login from "./pages/Login";
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {ProductList} from "./pages/ProductList"
+import Home from "./pages/Home"
+import Product from "./pages/Product"
+import Register from "./pages/Register"
+import {Cart} from "./pages/Cart"
+import Login from "./pages/Login"
+import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom'
 
 const App = () => {
+    const user = true
     return (
-        <Router>
+        <BrowserRouter>
             <Switch>
-                <Route path="/">
-                    <Home />
-                </Route>
-                <Route path="/product/:category">
-                    <ProductList />
-                </Route>
-                <Route path="/product/:id">
-                    <Product />
-                </Route>
-                <Route path="/cart">
-                    <Cart />
-                </Route>
+                <Route path="/products/:category" component={ProductList}/>
+                <Route path="/cart" component={Cart}/>
+                <Route path="/product/:id" component={Product}/>
                 <Route path="/login">
+                    {user ? <Redirect path="/"/> : <Login/>}
                     <Login />
-                </Route>
+                    </Route>
                 <Route path="/register">
+                    {user ? <Redirect path="/register"/> : <Register/>}
                     <Register />
                 </Route>
+                <Route exact={true} path="/" component={Home}/>
             </Switch>
-        </Router>
+        </BrowserRouter>
     )
 };
 
