199a1f9936c18deef4d1a9e103cad0235a7f3d3c
Aldino Pratama <aldinopratama15@gmail.com>
2021-10-29 15:14:51 +0700

    update app file

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,4 +1,9 @@
+import ProductList from "./pages/ProductList";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import { Register } from "./pages/Register";
+import { Cart } from "./pages/Cart";
+import { Login } from "./pages/Login";
 
 const App = () => {
     return <Home />;
